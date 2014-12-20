a3753a232a118df3934605f8d8aec9a59e4c7bad
Alex Wiltschko <alex.bw@gmail.com>
2014-12-20 16:46:13 -0500

    version bump for all the new features

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ dependencies = ['click','requests', 'tabulate', 'whetlab']
 
 setup(
 	name='whetlab',
-	version='0.2.1.7',
+	version='0.2.2',
 	description='Whetlab client for Python',
         long_description=open('README.md').read(),
 	author='Whetlab LLC',
